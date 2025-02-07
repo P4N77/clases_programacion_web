e39f15e9172fcad0d82cb30d0f6c505100318446
German <germanm1853@gmail.com>
2025-02-06 21:33:38 -0500

    avances

---
+++ scripts/main.js
@@ -0,0 +1,35 @@
+alert("Hola mundo");
+
+const HOLA = "Hola mundo"
+console.log(HOLA);
+
+console.log([1,2,3,4,5]);
+
+let obj ={
+    nombre: "German",
+    profesion: "estudiante",
+};
+
+console.log(obj);
+
+console.table([1,2,3,4,5]);
+
+console.error("algo salio mal");
+
+console.warn("eso no esta permitido");
+
+let categoria = "Computadores",
+    marca = "Lenovo",
+    calificacion = 5
+
+console.log(categoria);
+console.log(calificacion);
+console.log(marca);
+
+
+const PRODUCTO ="monitor";
+const PRECIO = "30 dolares";
+
+console.log(PRODUCTO.length);
+console.log(PRODUCTO.indexOf("pulgadas"));
+
