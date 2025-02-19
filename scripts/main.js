ac9abd80aec9b658e7daefc6db80f3848df82d56
german <germanm1853@gmail.com>
2025-02-18 19:54:35 -0500

    proyecto wheater report

--- scripts/main.js
+++ scripts/main.js
@@ -1,35 +1,60 @@
-alert("Hola mundo");
+$(document).ready(function() {
+    // espera a que el documento este totalmente cargado
+    // constante con mi api key
+    const apiKey = '3b5accbcce6e3e2a8a33da60a746dfc3';
 
-const HOLA = "Hola mundo"
-console.log(HOLA);
+    // constantes con los codigos de los paises
+    const codigosPais = {
+        "Argentina": "AR",
+        "España": "ES",
+        "México": "MX",
+        "Francia": "FR",
+        "EEUU": "US"
+    };
 
-console.log([1,2,3,4,5]);
+    $('#consultarClima').click(function() {
+        // captura los dos valores del input y el select y los guarda en dos constantes
+        const pais = $('#pais').val();
+        const ciudad = $('#ciudad').val();
 
-let obj ={
-    nombre: "German",
-    profesion: "estudiante",
-};
+        // si no se ingresa una ciudad, devolvera un mensaje
+        if (!ciudad) {
+            $('#resultado').html('<p>Por favor, ingresa una ciudad.</p>');
+            return;
+        }
 
-console.log(obj);
+        // Verificar si la ciudad corresponde al país seleccionado
+        const codigoPais = codigosPais[pais];
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${codigoPais}&appid=${apiKey}&units=metric&lang=es`;
 
-console.table([1,2,3,4,5]);
+        $.get(url, function(data) {
+            // se captura la informacion traida por la api 
+            const descripcion = data.weather[0].description;
+            const temperatura = data.main.temp;
+            const humedad = data.main.humidity;
 
-console.error("algo salio mal");
+            // se genera una constante que tendra contenido html
+            const resultado = `
+                <h3>Clima en ${ciudad}, ${pais}</h3>
+                <p><strong>Descripción:</strong> ${descripcion}</p>
+                <p><strong>Temperatura:</strong> ${temperatura} °C</p>
+                <p><strong>Humedad:</strong> ${humedad} %</p>
+            `;
+            // se envia el resultado al contendor de resultado
+            $('#resultado').html(resultado);
 
-console.warn("eso no esta permitido");
+        }).fail(function(jqXHR,) {
+            // Si la ciudad no existe o hay otro error
+            let mensajeError = 'Error al obtener la información.';
 
-let categoria = "Computadores",
-    marca = "Lenovo",
-    calificacion = 5
+            if (jqXHR.status === 404) {
+                // si el error que captura el objeto de respuesta es 404 (osea no existe) generara error
+                mensajeError = 'La ciudad o el país ingresado no existe. Por favor, verifica la información y vuelve a intentarlo.';
+            } else {
+                mensajeError = `Error`;
+            }
 
-console.log(categoria);
-console.log(calificacion);
-console.log(marca);
-
-
-const PRODUCTO ="monitor";
-const PRECIO = "30 dolares";
-
-console.log(PRODUCTO.length);
-console.log(PRODUCTO.indexOf("pulgadas"));
-
+            $('#resultado').html(`<p>${mensajeError}</p>`);
+        });
+    });
+});
